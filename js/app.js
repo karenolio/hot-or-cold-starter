b84d75c1b273f2f9d3f49209b719ad78a6b79cfb
Karen Ng <karenkn52@Macintosh.local>
2014-08-01 14:52:09 -0400

    almost done
    there are some bugs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
+/* MY HOT OR COLD APP */
+
 
 $(document).ready(function(){
 	
+	/*--- Variable Declarations ---*/
+	var randomNumber;
+	var guessFlag;
+	var guessCount;
+	var userChoice;
+	var found = false;
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +21,127 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+/*---Creating a new Game ---*/
+	newGame();
+
+/*On submit*/
+$("form").submit(function(event){
+	event.preventDefault(); /*This happens prior to the actual submission, so we can cancel the submit action by calling .preventDefault() on the event object or by returning false from our handler. We can trigger the event manually when another element is clicked:*/
+
+	if (!found){
+		userChoice = $("#userGuess").val();
+		console.log("User choice = " + userChoice);
+		clearText(); /*how does it know i want to clear that search bar?*/
+		setFocus(); /*what is this for in our code?*/
+		guessFlag = checkChoice(userChoice); 
+			if (!guessFlag) { /*means, if guess is wrong do the following*/
+				guessCount++;
+				setCount(guessCount);
+				$("ul#guessList li").append("<li>" + userChoice + "</li>");
+				guessFlag = checkTemperature(Math.abs(randomNumber - userChoice));
+			};
+	} else {
+		setFeedback ("You won! Start a new game now!");
+	};		
 });
 
+/*Create New game when you click +NEW GAME*/
+$(".new").click(function(event){
+	event.preventDefault();
+	newGame();
+});
 
+/*newGame function - everything is cleared, everything goes back to zero*/
+/*why can't I use: var newGame = function () instead of function newGame() {*/
+	function newGame(){ 
+		guessFlag = true;
+		guessCount = 0;
+		$("ul#guessList li").remove();
+		setFeedback("Make your Guess!");
+		setCount(guessCount);
+		randomNumber=generateNumber;
+		setFocus();
+		clearText;
+	}
+/*-----------------setting up functions--------------------------------*/
+/* Generate Random Number */
+function generateNumber() {
+
+	var generatedNumber = Math.floor((Math.random()*100)+1);
+	console.log("Generated Random Number = "+ generatedNumber);
+
+	return generatedNumber;
+} /*Math.floor returns x, rounded downwards to the nearest integer*/
+
+/*set focus to the inputbox*/
+function setfocus(){
+	document.getElementById("userGuess").focus();
+}
+
+/*clears the inputbox*/
+function clearText (){
+	$("#userGuess").val('');
+}
+
+/*set guessCount - not sure i understand the code here*/ 
+function setCount (count) {
+	$('#count').text(guessCount);
+}
+
+/*--- Prompt for User's Guess ---*/
+	function getChoice() {
+		var userChoice = prompt("Guess the Number","Your Guess");
+		console.log("User Choice = "+ userChoice);
+		return userChoice;
+	}	
+
+/*--- Check if the User's Guess meets the rules---*/
+	function checkChoice(userChoice) {
+		if (isNaN(userChoice)) {
+			setFeedback("I accept only numbers.");
+			return true;
+		} else if (userChoice < 1 || userChoice > 100) {
+			setFeedback("Oops! Your guess has to be a number between 1 and 100!");
+			return true;
+		}else if ($.trim(userChoice) == '') {
+			setFeedback("Enter your guess!");
+			return true;
+		} else {
+			return false;
+		};
+	}
+
+/*--- DONE: Providing temperature as feedback for user---*/
+	function checkTemperature(guessDifference){
+		if (guessDifference == 0) {
+			setFeedback("Yay!");
+			found = true;
+			return false;
+		} else if (guessDifference <= 5) {
+			setFeedback("Getting too hot!");
+			return true;
+		} else if (guessDifference <= 10){
+			setFeedback("Getting hot!");
+			return true;
+		} else if (guessDifference>=10 && guessDifference <= 20) {
+			setFeedback("Guess is getting Warm!");
+			return true;
+		} else if (guessDifference>=20 && guessDifference <= 30) {
+			setFeedback("Guess is getting cold!");
+			return true;
+		} else if (guessDifference>=30 && guessDifference <= 40) {
+			setFeedback("Guess is getting very cold!");
+			return true;
+		} else {
+			setFeedback("Guess is freezing cold!");
+			return true;
+		}
+	}
+
+	/*--- Set the feedback ---*/
+	function setFeedback(feedback) {
+		$('#feedback').text(feedback);
+	}
+
+
+});
